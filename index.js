8433a0f1c549f432e827f2ccdb6d271a170e4d31
JarrahGreen <dylan1080g@gmail.com>
2025-03-27 12:21:39 +0000

    Framework of website

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log('Happy developing ✨')
+const timetables = {
+    school1: [
+        { date: "20/03/2025", time: "9:00 AM", class: "Box fit" },
+        { date: "22/03/2023", time: "10:00 AM", class: "Technical" },
+    ],
+    school2: [
+        { date: "20/03/2025", time: "9:00 AM", class: "Box fit" },
+        { date: "22/03/2023", time: "10:00 AM", class: "Technical" },
+    ],
+    school3: [
+        { date: "20/03/2025", time: "9:00 AM", class: "Box fit" },
+        { date: "22/03/2023", time: "10:00 AM", class: "Technical" },
+    ],
+    school4: [
+        { date: "20/03/2025", time: "9:00 AM", class: "Box fit" },
+        { date: "22/03/2023", time: "10:00 AM", class: "Technical" },
+    ],
+};
+
+function loadTimetable() {
+    const school = document.getElementById("schoolSelect").value;
+    const timetable = timetables[school];
+    const tbody = document.querySelector("#timetable tbody");
+    tbody.innerHTML = "";
+
+    timetable.forEach(entry => {
+        const row = document.createElement("tr");
+        row.innerHTML = `<td>${entry.date}</td><td>${entry.time}</td><td class="available" onclick="bookClass(this)">${entry.class}</td>`;
+        tbody.appendChild(row);
+    });
+}
+
+function bookClass(cell) {
+    if (cell.classList.contains("available")) {
+        cell.classList.remove("available");
+        cell.classList.add("booked");
+        cell.innerHTML += " (Booked)";
+    }
+}
+
+window.onload = loadTimetable;
