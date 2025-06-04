57277fcf718633ec53024c9fba5812506d4dac6d
JarrahGreen <dylan1080g@gmail.com>
2025-06-04 14:44:09 +0100

    update server.js

--- stripe-backend/server.js
+++ stripe-backend/server.js
@@ -28,6 +28,7 @@ app.post('/create-checkout-session', async (req, res) => {
             mode: 'payment',
             success_url: 'http://localhost:5500/success.html',
             cancel_url: 'http://localhost:5500/cancel.html',
+            allow_promotion_codes: true,
         });
 
         res.json({ url: session.url });
